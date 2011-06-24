18ff01c974d13801641fbde336b551fa36d807a7
daserzw <daserzw@gmail.com>
2011-06-24 17:45:10 +0200

    init

---
+++ vagbros_pong/src/vagbros_pong.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : vagbros_pong.c
+ Author      : vagbros
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Pong
+ ============================================================================
+ */
+
+
+int main(int argc, char **argv) {
+
+}
+
